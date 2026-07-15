b59d68fb34a30567a6cfbc91dd8f892f031b9868
Osamsami <osamsamiawan0465@gmail.com>
2026-07-15 19:19:09 +0500

    feat: Build transactions CRUD API
    Closes #22

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,1 +1,10 @@
-// Transaction routes
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
+
+router.get('/', transactionController.getTransactions);
+router.post('/', transactionController.addTransaction);
+router.put('/:id', transactionController.updateTransaction);
+router.delete('/:id', transactionController.deleteTransaction);
+
+module.exports = router;
